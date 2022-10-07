2799f97b32d312fb3df812ad1c82c319fb2bd747
Carl Frenn <carl.frenn@gmail.com>
2022-10-07 10:43:44 +0200

    updated the date picker

--- src/components/form/formsubcomponents/BeautifulDatePicker.tsx
+++ src/components/form/formsubcomponents/BeautifulDatePicker.tsx
@@ -3,15 +3,43 @@ import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers'
 import { TextField } from '@mui/material'
 import { minWidth } from '../ContactForm'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { CalendarToday } from '@mui/icons-material'
+
+const popperSx = {
+  '& .MuiPaper-root': {
+    color: 'yellow',
+  },
+  '& [role=grid]': {
+    backgroundColor: 'rgba(0,0,0,0.1)',
+    '& button': {
+      backgroundColor: 'rgba(0,0,0,0.2)',
+    },
+  },
+}
 
 const BeautifulDatePicker = (props: any) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DesktopDatePicker
         {...props}
+        label="Date"
+        views={['day']}
         inputFormat="YYYY-MM-DD"
         renderInput={(params) => {
           return <TextField {...params} sx={{ minWidth: minWidth }} />
+        }}
+        InputProps={{
+          sx: {
+            '& .MuiSvgIcon-root': {
+              color: 'primary.main',
+            },
+          },
+        }}
+        components={{
+          OpenPickerIcon: CalendarToday,
+        }}
+        PopperProps={{
+          sx: popperSx,
         }}
         //   onChange={handleDatePickerChange}
         //   value={formValues.startDate}
