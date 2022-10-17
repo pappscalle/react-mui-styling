9620c1c522ab1fe81d591f5c0b946fbe3ffca26c
Carl Frenn <carl.frenn@gmail.com>
2022-10-17 15:30:13 +0200

    updated the breakpoints

--- src/theme/BeautifulTheme.ts
+++ src/theme/BeautifulTheme.ts
@@ -55,6 +55,15 @@ const BeautifulTheme = createTheme({
       ],
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
 })
 
 export default BeautifulTheme
